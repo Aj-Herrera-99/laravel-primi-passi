3e03af50726a6a09e5301228749c0735c9c801aa
Aj-Herrera-99 <ajherrera999@gmail.com>
2025-03-26 13:45:05 +0100

    creazione pagina homepage

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+
+    <title>Primi passi con Laravel</title>
+</head>
+
+<body>
+    <h1>Homepage</h1>
+    <h2>Hello world!</h2>
+</body>
+
+</html>
